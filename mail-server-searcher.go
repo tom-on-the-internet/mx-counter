66622b0bd82cd0d6fe99a76e23ef0437a3b89a34
Thomas Steven <thomaspatricksteven@gmail.com>
2020-11-12 22:51:45 -0500

    Initial code

---
+++ mail-server-searcher.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type kv struct {
+	Key   string
+	Value int
+}
+
+func main() {
+	emails, err := readEmails()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	emails = unique(emails)
+	emails = valid(emails)
+	domains := uniqueDomains(emails)
+	mailDomains := getMailDomains(domains)
+	domainCounts := getDomainCounts(emails, mailDomains)
+	orderedCounts := getOrderedCounts(domainCounts)
+
+	render(orderedCounts)
+
+	os.Exit(0)
+}
+
+func readEmails() ([]string, error) {
+	file, err := os.Open("emails.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, nil
+}
+
+func unique(emails []string) []string {
+	keys := make(map[string]bool)
+	uniqueEmails := []string{}
+
+	for _, email := range emails {
+		if _, value := keys[email]; !value {
+			keys[email] = true
+
+			uniqueEmails = append(uniqueEmails, email)
+		}
+	}
+
+	return uniqueEmails
+}
+
+func valid(emails []string) []string {
+	var validEmails []string
+
+	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+	for _, email := range emails {
+		if len(email) > 2 && len(email) < 255 && emailRegex.MatchString(email) {
+			validEmails = append(validEmails, email)
+		}
+	}
+
+	return validEmails
+}
+
+func getDomain(email string) string {
+	return strings.Split(email, "@")[1]
+}
+
+func uniqueDomains(emails []string) []string {
+	domains := make([]string, len(emails))
+
+	for i, email := range emails {
+		domains[i] = getDomain(email)
+	}
+
+	return unique(domains)
+}
+
+func getMailDomains(domains []string) map[string]string {
+	m := make(map[string]string)
+
+	for _, domain := range domains {
+		mxs, err := net.LookupMX(domain)
+		if err != nil {
+			continue
+		}
+
+		minimumDomainParts := 3
+
+		parts := strings.Split(mxs[0].Host, ".")
+		if len(parts) < minimumDomainParts {
+			continue
+		}
+
+		m[domain] = parts[len(parts)-3] + "." + parts[len(parts)-2]
+	}
+
+	return m
+}
+
+func getDomainCounts(emails []string, mailDomains map[string]string) map[string]int {
+	m := make(map[string]int)
+
+	for _, email := range emails {
+		domain := getDomain(email)
+		mailDomain := mailDomains[domain]
+		m[mailDomain]++
+	}
+
+	return m
+}
+
+func getOrderedCounts(domainCounts map[string]int) []kv {
+	orderedCounts := make([]kv, 0, len(domainCounts))
+
+	for k, v := range domainCounts {
+		orderedCounts = append(orderedCounts, kv{k, v})
+	}
+
+	sort.Slice(orderedCounts, func(i, j int) bool {
+		return orderedCounts[i].Value > orderedCounts[j].Value
+	})
+
+	return orderedCounts
+}
+
+func render(orderedCounts []kv) {
+	for _, count := range orderedCounts {
+		fmt.Println(count.Key, count.Value)
+	}
+}
